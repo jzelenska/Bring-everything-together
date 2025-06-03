24a1c55e47f604f6ede7f1558f03eba1360cedea
jzelenska <julia.zelenska@gmail.com>
2025-06-03 15:19:30 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
+
 var MyName = document.getElementById("name")
 var MyAge = document.getElementById("date")
 var MyEmail = document.getElementById("email")
 var MyEducation = document.getElementById("Education")
-var CompletedHigherEdu = document.getElementById("radio1")
-var NoHigherEdu = document.getElementById("radio2")
+var CompletedHigherEdu = document.getElementById("radioDefault1")
+var NoHigherEdu = document.getElementById("radioDefault2")
 var Economics = document.getElementById("checkbox1")
 var Sociology = document.getElementById("checkbox2")
 var Biology = document.getElementById("checkbox3")
 var Statistics = document.getElementById("checkbox4")
 var IT = document.getElementById("checkbox5")
+let i = 1;
 
 
 console.log(MyName, MyAge, MyEmail, MyEducation, CompletedHigherEdu, NoHigherEdu)
@@ -37,3 +39,17 @@ function validateForm() {
     
     return true;
 }
+
+
+for (let index = 0; index < i; index++) {
+    const element = array[index];
+}
+console.log(element);
+
+var submitted = document.getElementById("done");
+let message = document.getElementById("thanks")
+submitted.addEventListener("click", function() {
+    i = i+1;
+}
+)
+console.log(i);
